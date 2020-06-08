eab5ba02ceed86adc9da625bfae24797bb8677e4
sunjinping <sunjp@getui.com>
2020-06-08 16:34:56 +0800

    测试提交

--- spring-boot-banner/src/main/java/com/neo/banner/BannerApplication.java
+++ spring-boot-banner/src/main/java/com/neo/banner/BannerApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BannerApplication {
 
+
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BannerApplication.class, args);
 	}
